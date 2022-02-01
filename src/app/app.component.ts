4d4342a11555e03034fa2538dc3940eca77c9bd6
paul <paul.burge8@gmail.com>
2022-02-01 05:33:38 -0800

    added bootstrap/jquery and removed the default localhost page. also added some small stuff just to add it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'flopbuster2';
+  title = 'FlopBuster';
+
+  movieList = {
+	  count:1
+  }
+
+  theRoom = { 
+	title:"The Room"
+  }
+
+
 }
